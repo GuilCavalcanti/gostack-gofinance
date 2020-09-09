b877c2521326c87543fcddca53f359a659d739ed
GuilCavalcanti <58377529+GuilCavalcanti@users.noreply.github.com>
2020-09-09 21:11:10 +0100

    Done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
+// import filesize from 'filesize';
 
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
@@ -20,22 +20,25 @@ interface FileProps {
 
 const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
+  // const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const { name, size } = files[0];
+    const file = files[0];
+    const readableSize = size.toString();
+    setUploadedFiles([...uploadedFiles, { file, name, readableSize }]);
   }
 
   return (
